49b6c9e61f4d65511a96861959cf7eedc0875277
nayana027 <162151398+nayana027@users.noreply.github.com>
2025-02-21 10:50:37 -0500

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is a sample portfolio website deployed with CI/CD.</p>
+    </div>
+  );
+}
+
+export default App;
